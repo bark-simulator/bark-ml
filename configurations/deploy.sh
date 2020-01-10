8809633a60df0257221028d3359526cf029cba38
Patrick <patrickhart.1990@gmail.com>
2020-01-10 22:51:24 +0100

    Cluster improvements

--- configurations/deploy.sh
+++ configurations/deploy.sh
@@ -3,6 +3,6 @@
 echo "Building $.."
 bazel build //configurations/$1:configuration
 echo "Uploading.."
-# rsync bazel-bin/configurations/$1/ 8gpu:/mnt/glusterdata/home/$2/$3 -a --copy-links -v -z -P
-rsync bazel-bin/configurations/$1/ ../experiment -a --copy-links -v -z -P
+rsync bazel-bin/configurations/$1/ 8gpu:/mnt/glusterdata/home/$2/$3 -a --copy-links -v -z -P
+rsync ./configurations/run.sh 8gpu:/mnt/glusterdata/home/$2/$3/run.sh -a --copy-links -v -z -P
 echo "Uploaded!"
--- configurations/run.sh
+++ configurations/run.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-#SBATCH --qos lowprio
+#SBATCH --qos normal
 #SBATCH -c 4
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10GB
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root."
-  exit
-fi
-
-srun singularity exec --nv -B .:/experiment /mnt/glusterdata/home/hart/images/bark_ml.img python3 -u ./$1.runfiles/configuration 
+srun singularity exec --nv ../images/bark_ml.img python3 -u ./configuration 
