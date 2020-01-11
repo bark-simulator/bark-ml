edfec116f5db1cce9af3c92f04aee36280a469e9
Patrick Hart <patrickhart.1990@gmail.com>
2020-01-11 11:14:26 +0100

    Changed runner so results can be viewed

--- src/runners/tfa_runner.py
+++ src/runners/tfa_runner.py
@@ -42,8 +42,11 @@ class TFARunner(BaseRunner):
     self._summary_writer = None
     self._unwrapped_runtime = unwrapped_runtime
     if self._params["ML"]["Runner"]["summary_path"] is not None:
-      self._summary_writer = tf.summary.create_file_writer(
-        self._params["BaseDir"] + "/" + self._params["ML"]["Runner"]["summary_path"])
+      try:
+        self._summary_writer = tf.summary.create_file_writer(
+          self._params["BaseDir"] + "/" + self._params["ML"]["Runner"]["summary_path"])
+      except:
+        pass
     self.get_initial_collection_driver()
     self.get_collection_driver()
 
