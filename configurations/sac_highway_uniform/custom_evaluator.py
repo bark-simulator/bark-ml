47d5985cfaee4e3c02f153238046be5a1ab2a38a
Patrick Hart <patrickhart.1990@gmail.com>
2020-01-15 17:02:20 +0100

    Minor improvements

--- configurations/sac_highway_uniform/custom_evaluator.py
+++ configurations/sac_highway_uniform/custom_evaluator.py
@@ -1,7 +1,7 @@
 import numpy as np
 from bark.world.evaluation import \
   EvaluatorGoalReached, EvaluatorCollisionEgoAgent, \
-  EvaluatorCollisionDrivingCorridor, EvaluatorStepCount
+  EvaluatorCollisionDrivingCorridor, EvaluatorStepCount, EvaluatorDrivableArea
 from modules.runtime.commons.parameters import ParameterServer
 from bark.geometry import *
 
@@ -23,6 +23,7 @@ class CustomEvaluator(GoalReached):
     self._evaluators["ego_collision"] = \
       EvaluatorCollisionEgoAgent(self._eval_agent)
     self._evaluators["step_count"] = EvaluatorStepCount()
+    self._evaluators["drivable_area"] = EvaluatorDrivableArea()
 
   def _distance_to_center_line(self, world):
     """calculates the distance of the agent
@@ -52,12 +53,14 @@ class CustomEvaluator(GoalReached):
     distance = self._distance_to_center_line(world)
     collision = eval_results["ego_collision"]
     step_count = eval_results["step_count"]
+    drivable_area = eval_results["drivable_area"]
     # determine whether the simulation should terminate
-    if success or collision or step_count > self._max_steps:
+    if success or collision or step_count > self._max_steps or drivable_area:
       done = True
     # calculate reward
     reward = collision * self._collision_penalty + \
-      success * self._goal_reward - 0.1*distance
+      success * self._goal_reward - 0.1*distance + \
+      drivable_area * self._collision_penalty
     return reward, done, eval_results
     
 
