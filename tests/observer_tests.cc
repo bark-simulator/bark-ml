830ae2b8a00d58475469624cc1988224a7d6dcfd
Patrick <patrickhart.1990@gmail.com>
2020-02-14 21:04:44 +0100

    Setup c++ environment

---
+++ tests/observer_tests.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 fortiss GmbH, Patrick Hart, Julian Bernhard, Klemens Esterle, Tobias Kessler
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+
+#include "gtest/gtest.h"
+#include "modules/commons/params/params.hpp"
+#include "src/observers/nearest_observer.hpp"
+
+TEST(observes, nearest_observer) {
+  using modules::commons::ParamsPtr;
+  
+}
