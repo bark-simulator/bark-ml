77243a72b47c5aed5652eafed2ec7bdcbe9c2b38
Patrick <patrickhart.1990@gmail.com>
2020-05-15 21:27:23 +0200

    Added tests; changed docker image

--- bark_ml/tests/py_behavior_tests.py
+++ bark_ml/tests/py_behavior_tests.py
@@ -8,18 +8,29 @@ import unittest
 import numpy as np
 import os
 import matplotlib
-matplotlib.use('PS')
 import time
 
+from bark_ml.behaviors.cont_behavior import ContinuousMLBehavior
+from bark_ml.behaviors.discrete_behavior import DiscreteMLBehavior
 from modules.runtime.commons.parameters import ParameterServer
+from bark.models.dynamic import SingleTrackModel
+from bark.world import World, MakeTestWorldHighway
 
 
 class PyBehaviorTests(unittest.TestCase):
-  @unittest.skip
-  def test_runtime_rl(self):
-    params = ParameterServer(
-      filename="tests/data/deterministic_scenario_test.json")
-    pass
+  def test_discrete_behavior(self):
+    params = ParameterServer()
+    dynamic_model = SingleTrackModel(params)
+    discrete_behavior = DiscreteMLBehavior(dynamic_model, params)
+    # sets 0-th motion primitive active
+    discrete_behavior.ActionToBehavior(0)
+    
+  def test_cont_behavior(self):
+    params = ParameterServer()
+    cont_behavior = ContinuousMLBehavior(params)
+    # sets numpy array as next action
+    cont_behavior.ActionToBehavior(np.array([0., 0.]))
+
 
 if __name__ == '__main__':
   unittest.main()
--- bark_ml/tests/py_evaluator_tests.py
+++ bark_ml/tests/py_evaluator_tests.py
@@ -8,32 +8,30 @@ import unittest
 import numpy as np
 import os
 import matplotlib
-# matplotlib.use('PS')
 import time
 
+from bark_ml.behaviors.cont_behavior import ContinuousMLBehavior
+from bark_ml.behaviors.discrete_behavior import DiscreteMLBehavior
 from modules.runtime.commons.parameters import ParameterServer
 from bark_ml.environments.blueprints import ContinuousHighwayBlueprint, DiscreteHighwayBlueprint
 from bark_ml.environments.single_agent_runtime import SingleAgentRuntime
+from bark_ml.evaluators.goal_reached import GoalReached
 
 
-class PyEnvironmentTests(unittest.TestCase):
-  def test_env_cont_rl(self):
+class PyEvaluatorTests(unittest.TestCase):
+  def test_goal_reached_observer(self):
     params = ParameterServer()
     bp = ContinuousHighwayBlueprint(params)
     env = SingleAgentRuntime(blueprint=bp, render=True)
     env.reset()
-    for _ in range(0, 20):
-      observed_next_state, reward, done, info = env.step(np.array([1., 0.1]))
-      print(f"Reward: {reward}, Done: {done}")
+    world = env._world
 
-  def test_env_discrete_rl(self):
-    params = ParameterServer()
-    bp = DiscreteHighwayBlueprint(params)
-    env = SingleAgentRuntime(blueprint=bp, render=True)
-    env.reset()
-    for _ in range(0, 20):
-      observed_next_state, reward, done, info = env.step(1)
-      print(f"Reward: {reward}, Done: {done}")
+    eval_id = env._scenario._eval_agent_ids[0]
+    observed_world = world.Observe([eval_id])[0]
+    evaluator = GoalReached(params)
+    action = np.array([0., 0.], dtype=np.float32)
+    print(evaluator.Evaluate(observed_world, action))
+    
 
 if __name__ == '__main__':
   unittest.main()
