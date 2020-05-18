5a441fbd40d0df45b92838437cd7ccf86aae662e
Patrick Hart <patrickhart.1990@gmail.com>
2020-05-18 15:45:01 +0200

    Modified tfa examples

--- examples/tfa.py
+++ examples/tfa.py
@@ -6,6 +6,8 @@
 
 # TensorFlow Agents (https://github.com/tensorflow/agents) example
 import gym
+from absl import app
+from absl import flags
 
 # BARK imports
 from bark_project.modules.runtime.commons.parameters import ParameterServer
@@ -17,37 +19,51 @@ from bark_ml.library_wrappers.lib_tf_agents.agents import BehaviorSACAgent, Beha
 from bark_ml.library_wrappers.lib_tf_agents.runners import SACRunner, PPORunner
 
 
-# params = ParameterServer(filename="/Users/hart/2020/bark-ml/examples/tfa_params.json")
-params = ParameterServer()
-params["ML"]["BehaviorTFAAgents"]["CheckpointPath"] = "/home/hart/Dokumente/2020/bark-ml/checkpoints/"
-params["ML"]["TFARunner"]["SummaryPath"] = "/home/hart/Dokumente/2020/bark-ml/checkpoints/"
+FLAGS = flags.FLAGS
+flags.DEFINE_enum("mode",
+                  "visualize",
+                  ["train", "visualize", "evaluate"],
+                  "Mode the configuration should be executed in.")
 
 
-bp = ContinuousHighwayBlueprint(params,
-                                number_of_senarios=10,
-                                random_seed=0)
-
-env = SingleAgentRuntime(blueprint=bp,
-                         render=False)
-
-# ppo
-# ppo_agent = BehaviorPPOAgent(environment=env,
-#                              params=params)
-# env.ml_behavior = ppo_agent
-# runner = PPORunner(params=params,
-#                    environment=env,
-#                    agent=ppo_agent)
-
-# env = gym.make("Pendulum-v0")
-# sac
-sac_agent = BehaviorSACAgent(environment=env,
-                             params=params)
-env.ml_behavior = sac_agent
-runner = SACRunner(params=params,
-                   environment=env,
-                   agent=sac_agent)
+def run_configuration(argv):
+  # params = ParameterServer(filename="/Users/hart/2020/bark-ml/examples/tfa_params.json")
+  params = ParameterServer()
+  params["ML"]["BehaviorTFAAgents"]["CheckpointPath"] = "/home/hart/Dokumente/2020/bark-ml/checkpoints/"
+  params["ML"]["TFARunner"]["SummaryPath"] = "/home/hart/Dokumente/2020/bark-ml/checkpoints/"
 
 
-runner.Train()
-runner.Visualize(5)
-# params.save("/Users/hart/2020/bark-ml/examples/tfa_params.json")
+  # create environment
+  bp = ContinuousHighwayBlueprint(params,
+                                  number_of_senarios=10,
+                                  random_seed=0)
+  env = SingleAgentRuntime(blueprint=bp,
+                          render=False)
+
+  # PPO-agent
+  # ppo_agent = BehaviorPPOAgent(environment=env,
+  #                              params=params)
+  # env.ml_behavior = ppo_agent
+  # runner = PPORunner(params=params,
+  #                    environment=env,
+  #                    agent=ppo_agent)
+
+  # SAC-agent
+  sac_agent = BehaviorSACAgent(environment=env,
+                              params=params)
+  env.ml_behavior = sac_agent
+  runner = SACRunner(params=params,
+                    environment=env,
+                    agent=sac_agent)
+
+  if FLAGS.mode == "train":
+    runner.Train()
+  elif FLAGS.mode == "visualize":
+    runner.Visualize(5)
+  
+  # store all used params of the training
+  # params.save("/Users/hart/2020/bark-ml/examples/tfa_params.json")
+
+
+if __name__ == '__main__':
+  app.run(run_configuration)
