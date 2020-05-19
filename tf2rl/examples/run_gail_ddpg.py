f7a418c554007ef713d540565fdd1129d2486947
Marcel Brucker <Marcel@MacBook-Pro-2.local>
2020-05-19 15:49:01 +0200

    Incorporated tf2rl in the installation file and added th relevant example script (with minor modifications)

---
+++ tf2rl/examples/run_gail_ddpg.py
@@ -0,0 +1,45 @@
+import gym
+
+from tf2rl.algos.ddpg import DDPG
+from tf2rl.algos.gail import GAIL
+from tf2rl.experiments.irl_trainer import IRLTrainer
+from tf2rl.experiments.utils import restore_latest_n_traj
+
+
+if __name__ == '__main__':
+    parser = IRLTrainer.get_argument()
+    parser = GAIL.get_argument(parser)
+    parser.add_argument('--env-name', type=str, default="Pendulum-v0")
+    parser.add_argument('--gpu', type=int, default=0)  # was missing
+    args = parser.parse_args()
+
+    if args.expert_path_dir is None:
+        print("Plaese generate demonstrations first")
+        print("python examples/run_sac.py --env-name=Pendulum-v0 --save-test-path --test-interval=50000")
+        exit()
+
+    units = [400, 300]
+
+    env = gym.make(args.env_name)
+    test_env = gym.make(args.env_name)
+    policy = DDPG(
+        state_shape=env.observation_space.shape,
+        action_dim=env.action_space.high.size,
+        max_action=env.action_space.high[0],
+        gpu=args.gpu,
+        actor_units=units,
+        critic_units=units,
+        n_warmup=10000,
+        batch_size=100)
+    irl = GAIL(
+        state_shape=env.observation_space.shape,
+        action_dim=env.action_space.high.size,
+        units=units,
+        enable_sn=args.enable_sn,
+        batch_size=32,
+        gpu=args.gpu)
+    expert_trajs = restore_latest_n_traj(
+        args.expert_path_dir, n_path=20, max_steps=1000)
+    trainer = IRLTrainer(policy, env, args, irl, expert_trajs["obses"],
+                         expert_trajs["next_obses"], expert_trajs["acts"], test_env)
+    trainer()
