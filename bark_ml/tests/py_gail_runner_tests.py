3e1f69b0e186472d078f74a309cbba6244e1993c
Ferenc Török <ferike.trk@gmail.com>
2020-06-10 19:25:27 +0200

    params.json and filegroup in BUILD file is written for gail tests.

--- bark_ml/tests/py_gail_runner_tests.py
+++ bark_ml/tests/py_gail_runner_tests.py
@@ -1,7 +1,23 @@
+import unittest
 import gym
+
 
 # BARK imports
 from bark_project.modules.runtime.commons.parameters import ParameterServer
 
 # BARK-ML imports
-import bark_ml.library_wrappers.lib_tf2rl.runners.tf2rl_runner
+from bark_ml.library_wrappers.lib_tf2rl.runners.gail_runner import GAILRunner 
+
+class PyGAILRunnerTests(unittest.TestCase):
+
+    def test_initialization(self):
+        """tests the __init__() method of the GAILRunner class."""
+        env_name = "Pendulum-v0"
+        env = gym.make(env_name)
+
+        params = ParameterServer(filename="bark_ml/tests/gail_data/params/gail_params.json")
+        #runner = GAILRunner(params=params)
+
+
+if __name__ == '__main__':
+    unittest.main()
