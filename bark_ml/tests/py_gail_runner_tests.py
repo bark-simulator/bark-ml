84e262d01a9bb6dbf2c5bb0dda00d16e1e77d8f6
Ferenc Török <ferike.trk@gmail.com>
2020-06-10 15:22:08 +0200

    does not import bark_project.

---
+++ bark_ml/tests/py_gail_runner_tests.py
@@ -0,0 +1,7 @@
+import gym
+
+# BARK imports
+from bark_project.modules.runtime.commons.parameters import ParameterServer
+
+# BARK-ML imports
+import bark_ml.library_wrappers.lib_tf2rl.runners.tf2rl_runner
