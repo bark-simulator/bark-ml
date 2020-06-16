99aa077c3a83c49cdcf272452b7c4624d1a095ac
Marcel Brucker <63588974+marcelbrucker@users.noreply.github.com>
2020-06-16 16:07:18 +0200

    Implemented key functionalities of GAIL agent

--- bark_ml/library_wrappers/lib_tf2rl/agents/gail_agent.py
+++ bark_ml/library_wrappers/lib_tf2rl/agents/gail_agent.py
@@ -1,16 +1,25 @@
-import sys
-import logging
-import time
+# import sys
+# import logging
+# import time
 import tensorflow as tf
 tf.compat.v1.enable_v2_behavior()
 
-# tf2rl imports
+# tfa
+# from tf_agents.networks import actor_distribution_network
+# from tf_agents.networks import normal_projection_network
+from tf_agents.policies import greedy_policy
+
+# from tf_agents.replay_buffers import tf_uniform_replay_buffer
+# from tf_agents.utils.common import Checkpointer
+from tf_agents.trajectories import time_step as ts
+
+# tf2rl
 import tf2rl
 from tf2rl.algos.ddpg import DDPG
 from tf2rl.algos.gail import GAIL
 
-# BARK imports
 from bark_ml.library_wrappers.lib_tf2rl.agents.tf2rl_agent import BehaviorTF2RLAgent
+from bark_ml.library_wrappers.lib_tf_agents.agents.tfa_agent import BehaviorTFAAgent
 from bark_ml.behaviors.cont_behavior import BehaviorContinuousML
 
 class BehaviorGAILAgent(BehaviorTF2RLAgent, BehaviorContinuousML):
@@ -24,32 +33,57 @@ class BehaviorGAILAgent(BehaviorTF2RLAgent, BehaviorContinuousML):
                                     environment=environment,
                                     params=params)
     BehaviorContinuousML.__init__(self, params)
+    # self._replay_buffer = self.GetReplayBuffer()
+    # self._dataset = self.GetDataset()
+    # self._collect_policy = self.GetCollectionPolicy()
+    # self._eval_policy = self.GetEvalPolicy()
 
     self._generator = self._get_generator()
     self._discriminator = self._get_discriminator()
 
 
   def _get_generator(self):
-    """Instantiate DDPG generator here."""
-    pass
+    policy = DDPG(
+      state_shape=self._environment.observation_space.shape,
+      action_dim=self._environment.action_space.high.size,
+      max_action=self._environment.action_space.high[0],
+      gpu=self._params.["ML"]["Settings"]["GPUUse", "", 0],
+      actor_units=self._params.["ML"]["BehaviorGAILAgent"]["Generator"]["ActorFcLayerParams", "", [400, 300]],
+      critic_units=self._params.["ML"]["BehaviorGAILAgent"]["Generator"]["CriticJointFcLayerParams", "", [400, 300]],
+      n_warmup=self._params.["ML"]["BehaviorGAILAgent"]["WarmUp", "", 10000],
+      batch_size=self._params.["ML"]["BehaviorGAILAgent"]["Generator"]["BatchSize", "", 100])
+    return policy
 
   
   def _get_discriminator(self):
     """Instantiate discriminator network here."""
+    irl = GAIL(
+      state_shape=self._environment.observation_space.shape,
+      action_dim=self._environment.action_space.high.size, 
+      units=self._params.["ML"]["BehaviorGAILAgent"]["Discriminator"]["FcLayerParams", "", [400, 300]],
+      enable_sn=self._params.["ML"]["BehaviorGAILAgent"]["EnableSN", "", False],
+      batch_size=self._params.["ML"]["BehaviorGAILAgent"]["Discriminator"]["BatchSize", "", 32],
+      gpu=self._params.["ML"]["Settings"]["GPUUse", "", 0])
+    return irl
 
+  # def GetEvalPolicy(self):
+  #   return greedy_policy.GreedyPolicy(self._agent.policy)
 
   def Reset(self):
-    """Has to be implemented here"""
     pass
 
+  # @property
+  # def eval_policy(self):
+  #   return self._eval_policy
 
   def Act(self, state):
-    """Has to be implemented here"""
-    pass
+    return self._generator.get_action(state)
 
 
   def Plan(self, observed_world, dt):
-    """Has to be implemented here"""
+    """In SAC never called and additionally itself calls
+    unimplemented functions there so ommited
+    """
     pass
 
 
