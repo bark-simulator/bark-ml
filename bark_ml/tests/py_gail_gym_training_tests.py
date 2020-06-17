5ddc1a4a2cea0ff66f1883a076140d702946747a
Ferenc Török <ferike.trk@gmail.com>
2020-06-17 14:24:28 +0200

    agents BUILD file modified for bark_ml/behavior dependency. Agent initialization tested.

---
+++ bark_ml/tests/py_gail_gym_training_tests.py
@@ -0,0 +1,41 @@
+import unittest
+import gym
+import os
+
+# BARK imports
+from bark_project.modules.runtime.commons.parameters import ParameterServer
+
+# BARK-ML imports
+from bark_ml.library_wrappers.lib_tf2rl.runners.gail_runner import GAILRunner 
+from bark_ml.library_wrappers.lib_tf2rl.agents.gail_agent import BehaviorGAILAgent
+
+
+class PyTrainingGymTests(unittest.TestCase):
+
+    def test_initialization(self):
+        """tests the __init__() method of the GAILRunner class."""
+
+        params = ParameterServer(filename="bark_ml/tests/gail_data/params/gail_params.json")
+
+        if len(os.listdir(params["ML"]["GAILRunner"]["tf2rl"]["expert_path_dir"])) == 0:
+            print("No expert trajectories found, plaese generate demonstrations first")
+            print("python tf2rl/examples/run_sac.py --env-name=Pendulum-v0 --save-test-path --test-interval=50000")
+            print("After that, save expert trajectories into bark_ml/tests/gail_data/expert_data")
+            exit()
+
+        env_name = "Pendulum-v0"
+        env = gym.make(env_name) 
+
+        agent = BehaviorGAILAgent(
+            environment=env,
+            params=params
+        )
+        
+        runner = GAILRunner(
+            environment=env,
+            agent=agent,
+            params=params)
+
+
+if __name__ == '__main__':
+    unittest.main()
