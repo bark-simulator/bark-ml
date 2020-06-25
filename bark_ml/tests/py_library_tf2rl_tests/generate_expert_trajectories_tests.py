ca99cd6bce4d9e1236b20c3b70ee06f1e884bda6
Brucknem <mbruckner94@gmail.com>
2020-06-25 14:30:05 +0200

    Added tests for get_track_files and get_map_files

--- bark_ml/tests/py_library_tf2rl_tests/generate_expert_trajectories_tests.py
+++ bark_ml/tests/py_library_tf2rl_tests/generate_expert_trajectories_tests.py
@@ -39,8 +39,39 @@ class CalculateActionTests(unittest.TestCase):
             expected_action,
             calculate_action(observations[1], observations[0], timestamps[1], timestamps[0], 2.7))
 
+
+class GetMapAndTrackFilesTests(unittest.TestCase):
+    """
+    Tests: get_track_files and get_map_files
+    """
+
+    def test_get_track_files(self):
+        """
+        Test: get_track_files
+        """
+        interaction_data_set_mock_path = os.path.join(os.path.dirname(__file__), 'data/interaction_data_set_mock')
+        track_files = get_track_files(interaction_data_set_mock_path)
+
+        self.assertIs(len(track_files), 1)
+        self.assertIs(track_files[0].endswith('bark_ml/tests/py_library_tf2rl_tests/data/interaction_data_set_mock/DR_DEU_Merging_MT/tracks/vehicle_tracks_013.csv'), True)
+
+    def test_load_map_files(self):
+        """
+        Test: get_map_files
+        """
+        interaction_data_set_mock_path = os.path.join(os.path.dirname(__file__), 'data/interaction_data_set_mock')
+        map_files = get_map_files(interaction_data_set_mock_path)
+
+        self.assertIs(len(map_files), 1)
+        self.assertIs(map_files[0].endswith('bark_ml/tests/py_library_tf2rl_tests/data/interaction_data_set_mock/DR_DEU_Merging_MT/map/DR_DEU_Merging_MT_v01_shifted.xodr'), True)
+    
+    def test_interaction_data_set_path_invalid(self):
+        """
+        Test: The given path is invalid
+        """
+        map_files = get_map_files(os.path.dirname(__file__))
+        print(map_files)
+
+
 if __name__ == '__main__':
     unittest.main()
-
-
-# TODO Create tests
