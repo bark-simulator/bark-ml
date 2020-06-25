7368c45bd3204e574e31ffef41ec7ba0ce99844d
Brucknem <mbruckner94@gmail.com>
2020-06-25 14:49:59 +0200

    Added better ValueError to load save if not a dir

--- bark_ml/library_wrappers/lib_tf2rl/load_save_utils.py
+++ bark_ml/library_wrappers/lib_tf2rl/load_save_utils.py
@@ -4,7 +4,8 @@ def list_files_in_dir(dir_path: str, file_ending: str = '') -> list:
     """
     Lists all files in the given dir ending with the given ending.
     """
-    assert os.path.isdir(dir_path)
+    if not os.path.isdir(dir_path):
+        raise ValueError(f'Cannot list files in {dir_path}. Not a directory.')
     files = [f for f in os.listdir(
         dir_path) if os.path.isfile(os.path.join(dir_path, f))]
     files = [f for f in files if str(f).endswith(file_ending)]
@@ -16,7 +17,8 @@ def list_dirs_in_dir(dir_path: str, include_git: bool = False) -> list:
     """
     Lists all dirs in the given dir.
     """
-    assert os.path.isdir(dir_path)
+    if not os.path.isdir(dir_path):
+        raise ValueError(f'Cannot list directories in {dir_path}. Not a directory.')
     dirs = [f for f in os.listdir(
         dir_path) if not os.path.isfile(os.path.join(dir_path, f))]
     if not include_git:
