1880792b2694b4a66a3ed3da32523c469f61a60b
Silvan <48797503+lesilvan@users.noreply.github.com>
2020-07-10 11:39:49 +0200

    Update install.sh

--- utils/install.sh
+++ utils/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 virtualenv -p python3.7 ./bark_ml/python_wrapper/venv
 source ./bark_ml/python_wrapper/venv/bin/activate && pip install --no-cache-dir --upgrade --trusted-host pypi.org -r ./utils/docker/installers/requirements.txt
-pip3 install tf2_gnn networkx
+source ./bark_ml/python_wrapper/venv/bin/activate && pip install tf2_gnn networkx
