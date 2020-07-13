e9da9425fb2c57e50516844cd083148c961e490a
Tom Dörr <tom.doerr@tum.de>
2020-07-13 15:56:01 +0200

    Add tfa paths to config

--- default_config.py
+++ default_config.py
@@ -1,5 +1,9 @@
 tfa_gnn_checkpoint_path = '/bark/checkpoints' 
 tfa_gnn_summary_path = '/bark/summaries' 
+
+tfa_checkpoint_path = '/bark/checkpoints' 
+tfa_summary_path = '/bark/summaries' 
+
 supervised_learning_logs_path = '/bark/supervised_learning/logs'
 supervised_learning_data_path = '/bark/supervised_learning/data'
 
