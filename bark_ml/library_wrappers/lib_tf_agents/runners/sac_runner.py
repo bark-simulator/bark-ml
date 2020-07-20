6d2f7649a8231c237bd81cf4ec2b4140bcba6d6a
Marco Oliva <marco.oliva@intive.com>
2020-07-20 08:22:28 +0200

    Add tensorboard logs for episode collection and training durations

--- bark_ml/library_wrappers/lib_tf_agents/runners/sac_runner.py
+++ bark_ml/library_wrappers/lib_tf_agents/runners/sac_runner.py
@@ -40,14 +40,26 @@ class SACRunner(TFARunner):
     t = time.time()
 
     for i in range(0, self._number_of_collections):
+
       self._agent._training = True
       
       global_iteration = self._agent._agent._train_step_counter.numpy()
       tf.summary.experimental.set_step(global_iteration)
       
+      t0 = time.time()
       self._collection_driver.run()
       experience, _ = next(iterator)
+      
+      tf.summary.scalar("Episode Collection Duration", 
+                        time.time() - t0, 
+                        global_iteration)
+
+      t0 = time.time()      
       self._agent._agent.train(experience)
+      
+      tf.summary.scalar("Training Duration", 
+                        time.time() - t0, 
+                        global_iteration)
       
       if global_iteration % self._evaluation_interval == 0:
         iterations = f'{global_iteration-self._evaluation_interval}-{global_iteration}'
