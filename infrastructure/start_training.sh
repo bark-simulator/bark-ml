446da0b5a4845c172f2915b82958fa8cdf0fe9cd
Tom Dörr <tom.doerr@tum.de>
2020-07-24 20:54:20 +0200

    Update script

--- infrastructure/start_training.sh
+++ infrastructure/start_training.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+
+while [[ "$#" -gt 0 ]]
+do
+    case $1 in
+        -t|--timeout) timeout="$2"; shift;;
+        *) echo Unknown parameter "$1"; exit 1;; 
+    esac
+    shift
+done
+
+echo timeout: $timeout
+
+prepend_command=""
+if [[ $timeout != "" ]]
+then
+    prepend_command=$prepend_command"timeout $timeout "
+fi
+
+read 
 docker run -it --gpus all \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -v ~/.Xauthority:/home/root/.Xauthority \
@@ -13,7 +32,7 @@ source utils/dev_into.sh;
 pip install networkx tf2-gnn;
 while true;
         do
-        timeout 300s bazel run --jobs 24 //examples:tfa_gnn -- --mode=train;
+        '"$prepend_command "' bazel run --jobs 12 //examples:tfa_gnn -- --mode=train;
         sleep 0.1;
 done
 '
