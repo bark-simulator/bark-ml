069f0070130a27fcc98a017b60444fe3d7e3ac63
Tom Dörr <tom.doerr@tum.de>
2020-07-25 01:35:29 +0200

    Add support for template venv

--- infrastructure/set_up_new_env.sh
+++ infrastructure/set_up_new_env.sh
@@ -7,10 +7,13 @@ MARCO_SUMMARY_PATH="/Users/marco.oliva/Development/bark-ml_logs/summaries"
 DOCKER_CHECKPOINT_PATH="/bark/checkpoints"
 DOCKER_SUMMARY_PATH="/bark/summaries"
 FILE_WITH_PATHS_TO_REPLACE="./examples/tfa_gnn.py"
+TEMPLATE_VENV_DIR="template_venv/"
+BARK_ML_VENV_LOCATION="bark_ml/python_wrapper/"
 
 git clone git@github.com:mrcoliva/bark-ml.git $env_name
 cd $env_name
 git checkout $BASE_BRANCH
+cp -r ../"$TEMPLATE_VENV_DIR" $BARK_ML_VENV_LOCATION/"venv"
 
 docker run -it --gpus all \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
