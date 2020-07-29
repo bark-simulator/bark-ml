f668bd80e1ebdce8bd71f1ebbdd44a3d8b7f217d
Tom Dörr <tom.doerr@tum.de>
2020-07-29 23:33:05 +0200

    Update script

--- infrastructure/start_hyper_opt.sh
+++ infrastructure/start_hyper_opt.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -x
 trap cleanup EXIT
 
 HYPER_OPT_START_COMMAND="./start_training_docker.sh --agent hyperparameter_optimization --move_checkpoints --timeout 12h"
@@ -19,9 +18,17 @@ cleanup() {
     done
 }
 
-for i in {1..5}
+for i in {1..8}
 do
     launch_hyper_opt_instance
 done
 
-sleep 1000000000
+start_time=$(date +%s)
+
+while true
+do
+    uptime_in_seconds=$(( $(date +%s) - $start_time ))
+    uptime_in_hours=$(( $uptime_in_seconds / 3600 ))
+    echo "Uptime: "$uptime_in_hours"h"
+    sleep 1
+done
