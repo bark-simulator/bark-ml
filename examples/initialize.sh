6f04f54e05eaae92bbdccaf5e658335f706272f7
Brucknem <mbruckner94@gmail.com>
2020-08-06 18:49:53 +0200

    Removed archives

--- examples/initialize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone git@github.com:GAIL-4-BARK/large_data_store.git
-unzip large_data_store/bark-ml.zip
-mv bark-ml/expert_trajectories .
-mv bark-ml/pretrained_agents .
-rm -rf bark-ml
-rm -rf large_data_store
