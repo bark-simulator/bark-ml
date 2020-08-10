c0a9db2bfdb6cd8494914952091eeba23a90a85b
Silvan Wimmer <silvan.wimmer@tum.de>
2020-08-10 12:39:08 +0200

    Restruct supervised learning location

---
+++ bark_ml/tests/capability_GNN_actor/learner.py
@@ -0,0 +1,186 @@
+# Copyright (c) 2020 Patrick Hart, Julian Bernhard,
+# Klemens Esterle, Tobias Kessler
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+
+import logging
+import numpy as np
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import time
+import networkx as nx
+import tensorflow as tf
+import datetime
+
+# Supervised learning imports
+from supervised_learning.actor_nets import ConstantActorNet, RandomActorNet, \
+    get_GNN_SAC_actor_net, get_SAC_actor_net
+
+class Learner:
+	def __init__(self, model, train_dataset, test_dataset, log_dir):
+		"""Function to set up the Learner"""
+		self.model = model
+		self.train_dataset = train_dataset
+		self.test_dataset = test_dataset
+		self.log_dir = log_dir
+
+	def train(self, epochs=10, only_test=False, mode="Distribution"):
+		"""Function to train the model
+				
+				optional parameters:
+						epochs      :   int     #number of epochs 
+						only_test   :   bool    #if false: no real training, just tests
+						mode        :   str     #"Distribution" or sth else"""
+
+		self.loss_object = tf.keras.losses.MeanSquaredError()
+		self.optimizer = tf.keras.optimizers.Adam()
+		self.train_loss = tf.keras.metrics.Mean(name='train_loss')
+		self.test_loss = tf.keras.metrics.Mean(name='test_loss')
+		current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+		self.summary_writer = tf.summary.create_file_writer(self.log_dir+'/'+ current_time)
+
+		for epoch in range(epochs):
+      # Reset the metrics at the start of the next epoch
+      self.train_loss.reset_states()
+      self.test_loss.reset_states()
+      logging.info('Starting training in epoch '+str(epoch))
+      # Start real training with gradients or only evaluate
+      if only_test==False:
+        for inputs, labels in self.train_dataset:
+          self.train_step(inputs, labels, model=self.model, loss_object=self.loss_object,
+                          optimizer=self.optimizer, train_loss=self.train_loss, mode=mode)
+      elif only_test==True:
+        for test_inputs, test_labels in self.train_dataset:
+          self.test_step(test_inputs, test_labels, model=self.model, loss_object=self.loss_object,
+                         test_loss=self.train_loss, mode=mode)
+      # Evaluate on test_dataset
+      for test_inputs, test_labels in self.test_dataset:
+        self.test_step(test_inputs, test_labels, model=self.model, loss_object=self.loss_object,
+                       test_loss=self.test_loss, mode=mode)
+      # Histogram data
+      logging.info("starting to create histogram data")
+      true_results, preds, abs_losses = self.create_histogram_data(self.train_dataset, self.model,
+                                                                    mode=mode)
+
+      with self.summary_writer.as_default():
+          tf.summary.scalar('loss/train_loss', self.train_loss.result(), step=epoch)
+          tf.summary.scalar('loss/test_loss', self.test_loss.result(), step=epoch)
+          tf.summary.histogram("steering/labels", tf.constant(true_results[:,0]), step=epoch)
+          tf.summary.histogram("steering/predictions", tf.constant(preds[:,0]), step=epoch)
+          tf.summary.histogram("acceleration/labels", tf.constant(true_results[:,1]), step=epoch)
+          tf.summary.histogram("acceleration/predictions", tf.constant(preds[:,1]), step=epoch)
+          tf.summary.histogram("absolute_losses/steering", tf.constant(abs_losses[:,0]), step=epoch)
+          tf.summary.histogram("absolute_losses/acceleration", tf.constant(abs_losses[:,1]), step=epoch)
+
+      template = 'Epoch {}, Loss: {}, Test Loss: {}'
+      print(template.format(epoch + 1,
+                              self.train_loss.result(),
+                              self.test_loss.result()))
+
+	def visualize_predictions(self, dataset, title=None, mode="Distribution"):
+    # Show exemplary predictions of model
+    abs_losses = list()
+    predictions = list()
+    true_results = list()
+    for input_data, y_true in dataset:
+      y_pred = self.model(input_data, training=False, step_type=None, network_state=())
+      if mode=="Distribution":
+        y_pred = y_pred[0].mean()
+
+      predictions.extend(y_pred.numpy())
+      true_results.extend(y_true.numpy())
+      absolute_loss = y_true.numpy() - y_pred.numpy()
+      abs_losses.extend(absolute_loss)
+    
+    # Visualize predictions
+    abs_losses = np.array(abs_losses)
+    predictions = np.array(predictions)
+    true_results = np.array(true_results)
+    fig = self.create_figure(abs_losses, predictions, true_results, title)
+    plt.show()
+
+	@staticmethod
+	def create_figure(abs_losses, predictions, true_results, title=None):
+    colors = ['b','r'] 
+    fig, axes = plt.subplots(nrows=4)
+    fig.suptitle(title, fontsize=16)
+    # Hist of steering
+    y1 = true_results[:,0]
+    y2 = predictions[:,0]
+    __ = axes[0].hist([y1,y2], bins=100, color=colors)
+    axes[0].legend(["Label", "Prediction"])
+    #axes[0].set_xlim(-10,10) 
+    axes[0].set_ylabel("Counts")
+    axes[0].set_xlabel("Steering")
+    axes[0].legend(loc='upper right')
+
+    # Hist of acc
+    y3 = true_results[:,1]
+    y4 = predictions[:,1]
+    __ = axes[1].hist([y3, y4], bins=100, color=colors) 
+    axes[1].set_ylabel("Counts")
+    axes[1].set_xlabel("Acceleration")
+    axes[1].legend(["Label", "Prediction"])
+
+    # Hist of abs steer losses
+    y5 = abs_losses[:,0]
+    __ = axes[2].hist(y5, bins=200) 
+    axes[2].set_ylabel("Counts")
+    axes[2].set_xlabel("Absolute losses steering (True-Pred)")
+
+    # Hist of abs steer losses
+    y6 = abs_losses[:,1]
+    __ = axes[3].hist(y6, bins=200) 
+    axes[3].set_ylabel("Counts")
+    axes[3].set_xlabel("Absolute losses acceleration (True-Pred)")
+
+    fig.tight_layout() 
+    return fig
+
+	@staticmethod
+	def train_step(inputs, labels, model, loss_object, optimizer, train_loss, mode="Distribution"):
+    with tf.GradientTape() as tape:
+      predictions = model(inputs, training=True, step_type=None, network_state=())
+      if mode=="Distribution":
+        predictions = predictions[0].mean()
+      loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    train_loss(loss)
+
+	@staticmethod 
+	def test_step(inputs, labels, model, loss_object, test_loss, mode="Distribution"):
+    # training=False is only needed if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    predictions = model(inputs, training=False, step_type=None, network_state=())
+    if mode=="Distribution":
+      predictions = predictions[0].mean()
+    t_loss = loss_object(labels, predictions)
+    test_loss(t_loss)
+    #test_accuracy(labels, predictions)
+
+	@staticmethod
+	def create_histogram_data(dataset, model, mode="Distribution"):
+    abs_losses = list()
+    preds = list()
+    true_results = list()
+
+    # Create raw data
+    for input_data, y_true in dataset:
+      y_pred = model(input_data, training=False, step_type=None, network_state=())
+      if mode=="Distribution":
+        y_pred = y_pred[0].mean()
+
+      preds.extend(y_pred.numpy())
+      true_results.extend(y_true.numpy())
+      absolute_loss = tf.math.subtract(y_true, y_pred)
+      absolute_loss = y_true.numpy() - y_pred.numpy()
+      abs_losses.extend(absolute_loss)
+
+    abs_losses = np.array(abs_losses)
+    preds = np.array(preds)
+    true_results = np.array(true_results)
+    return true_results, preds, abs_losses
---
+++ bark_ml/tests/capability_GNN_actor/py_learn_actor_tests.py
@@ -0,0 +1,117 @@
+# Copyright (c) 2020 Patrick Hart, Julian Bernhard,
+# Klemens Esterle, Tobias Kessler
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+
+import unittest
+import pickle
+import logging
+import numpy as np
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import time
+import networkx as nx
+import tensorflow as tf
+import datetime
+
+# BARK imports
+from bark.runtime.commons.parameters import ParameterServer
+from bark.runtime.viewer.matplotlib_viewer import MPViewer
+from bark.runtime.viewer.video_renderer import VideoRenderer
+
+# BARK-ML imports
+from bark_ml.environments.blueprints import ContinuousHighwayBlueprint, \
+  ContinuousMergingBlueprint, ContinuousIntersectionBlueprint
+from bark_ml.environments.single_agent_runtime import SingleAgentRuntime
+from bark_ml.library_wrappers.lib_tf_agents.agents import BehaviorSACAgent, BehaviorPPOAgent, BehaviorGraphSACAgent
+from bark_ml.library_wrappers.lib_tf_agents.runners import SACRunner, PPORunner
+from bark_ml.observers.graph_observer import GraphObserver
+from supervised_learning.data_generation import DataGenerator
+
+# Supervised imports
+from supervised_learning.actor_nets import ConstantActorNet, RandomActorNet, \
+    get_GNN_SAC_actor_net, get_SAC_actor_net
+from supervised_learning.data_generation import DataGenerator
+from supervised_learning.data_handler import Dataset
+from supervised_learning.learner import Learner
+
+class PyGNNActorTests(unittest.TestCase):
+  def setUp(self):
+    ######################
+    #    Parameter       #
+    from config import supervised_learning_logs_path, supervised_learning_data_path
+    self.log_dir = supervised_learning_logs_path
+    self.epochs = 5
+    self.batch_size = 32
+    self.train_split = 0.8
+    self.data_path = supervised_learning_data_path
+    self.num_scenarios = 200
+    ######################
+
+    """Setting up the test case"""
+    params = ParameterServer(filename="examples/example_params/tfa_params.json")
+    params["ML"]["BehaviorTFAAgents"]["NumCheckpointsToKeep"] = None
+    params["ML"]["SACRunner"]["EvaluateEveryNSteps"] = 50
+    params["ML"]["BehaviorSACAgent"]["BatchSize"] = 32
+    params["ML"]["GraphObserver"]["AgentLimit"] = 8
+    params["ML"]["BehaviorGraphSACAgent"]["GNN"]["num_layers"] = 4
+    params["ML"]["BehaviorGraphSACAgent"]["GNN"]["hidden_dim"] = 256
+    params["World"]["remove_agents_out_of_map"] = False
+    
+    # Get dataset
+    self.observer = GraphObserver(params=self.params)
+    dataset = Dataset(self.data_path, self.observer, self.params, batch_size=self.batch_size,
+                      train_split=self.train_split, num_scenarios=self.num_scenarios)
+    dataset.get_datasets()
+    self.train_dataset = dataset.train_dataset
+    self.test_dataset = dataset.test_dataset
+
+    # Calculate some values about dataset
+    num_batches = len(list(self.train_dataset.as_numpy_iterator()))
+    first = True
+    for inputs, labels in self.train_dataset:
+      if first:
+        means = tf.math.reduce_mean(labels, axis=0)
+        mins = tf.math.reduce_min(labels, axis=0)
+        maxs = tf.math.reduce_max(labels, axis=0)
+        first = False
+      else:
+        means += tf.math.reduce_mean(labels, axis=0)
+        mins = tf.math.minimum(mins, tf.math.reduce_min(labels, axis=0))
+        maxs = tf.math.maximum(maxs, tf.math.reduce_max(labels, axis=0))
+    means = (means / num_batches).numpy()
+
+    # Get agent
+    self.actor_net = get_GNN_SAC_actor_net(self.num_scenarios, self.params, self.observer)
+    #self.actor_net = get_SAC_actor_net(self.num_scenarios, self.params, self.observer)
+    #self.actor_net = ConstantActorNet(constants=means)
+    #self.actor_net = RandomActorNet(low=[0, -0.4], high=[0.1, 0.4])
+
+    # Get supervised learner
+    self.supervised_learner = Learner(self.actor_net, self.train_dataset, self.test_dataset, self.log_dir)
+  
+  def test_actor_network(self):
+    # Evaluates actor net formalia
+    actor_net = self.actor_net
+    self.assertIsNotNone(actor_net)
+  
+  def test_training(self):
+    logging.info(self.actor_net.__class__==ConstantActorNet)
+    if (self.actor_net.__class__ == ConstantActorNet) or (self.actor_net.__class__==RandomActorNet):
+      mode = "Number"
+      only_test = True
+    else:
+      mode = "Distribution"
+      only_test = False
+
+    self.supervised_learner.train(epochs=self.epochs, only_test=only_test, mode=mode)
+    title1 = str(self.actor_net.__class__) + " TRAIN DATASET"
+    self.supervised_learner.visualize_predictions(self.train_dataset, title=title1, mode=mode)
+    title2 = str(self.actor_net.__class__) + " TEST DATASET"
+    self.supervised_learner.visualize_predictions(self.test_dataset, title=title2, mode=mode)
+
+if __name__ == '__main__':
+    unittest.main()
