8e14cad8ab4ae04a24f45b52cdb3f158070de958
Silvan Wimmer <silvan.wimmer@tum.de>
2020-08-10 13:01:37 +0200

    Revert useless changes

--- utils/install.sh
+++ utils/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 virtualenv -p python3.7 ./bark_ml/python_wrapper/venv
-source ./bark_ml/python_wrapper/venv/bin/activate && pip install --no-cache-dir --upgrade --trusted-host pypi.org -r ./utils/docker/installers/requirements.txt
+source ./bark_ml/python_wrapper/venv/bin/activate && pip install --no-cache-dir --upgrade --trusted-host pypi.org -r ./utils/docker/installers/requirements.txt 
