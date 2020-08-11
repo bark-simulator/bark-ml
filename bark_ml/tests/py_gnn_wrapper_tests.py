e1709f789e8e8f41abb7c9c47093b9ace84caf21
Silvan Wimmer <silvan.wimmer@tum.de>
2020-08-11 10:15:28 +0200

    Fix gnn_wrapper tests

--- bark_ml/tests/py_gnn_wrapper_tests.py
+++ bark_ml/tests/py_gnn_wrapper_tests.py
@@ -11,13 +11,13 @@ from bark.runtime.commons.parameters import ParameterServer
 from bark_ml.environments.blueprints import ContinuousHighwayBlueprint
 from bark_ml.environments.single_agent_runtime import SingleAgentRuntime
 from bark_ml.observers.graph_observer import GraphObserver
-from bark_ml.library_wrappers.lib_tf2_gnn import GNNWrapper
+from bark_ml.library_wrappers.lib_tf_agents.networks import GNNWrapper
 from bark_ml.library_wrappers.lib_tf_agents.agents import BehaviorGraphSACAgent
 from bark_ml.library_wrappers.lib_tf_agents.runners import SACRunner
 
 class PyGNNWrapperTests(unittest.TestCase):
 
-  def _mock_setup(self):
+  def test_setup(self):
     params = ParameterServer()
     params["ML"]["GraphObserver"]["AgentLimit"] = 4
     params["ML"]["SACRunner"]["NumberOfCollections", "", 1]
@@ -25,7 +25,8 @@ class PyGNNWrapperTests(unittest.TestCase):
     params["ML"]["BehaviorGraphSACAgent"]["ActorFcLayerParams"] = [256, 128]
     params["ML"]["BehaviorGraphSACAgent"]["GNN"]["NumMpLayers"] = 2
     params["ML"]["BehaviorGraphSACAgent"]["GNN"]["MpLayerNumUnits"] = 256
-    params["ML"]["BehaviorGraphSACAgent"]["GNN"]["library"] = "spektral" # "tf2_gnn" or "spektral"
+    gnn_library = GNNWrapper.SupportedLibrary.spektral # "tf2_gnn" or "spektral"
+    params["ML"]["BehaviorGraphSACAgent"]["GNN"]["library"] = gnn_library
     params["ML"]["SACRunner"]["NumberOfCollections"] = int(1e6)
     params["ML"]["BehaviorGraphSACAgent"]["GNN"]["GraphDimensions"] = (4, 11, 4) # (n_nodes, n_features, n_edge_features)
 
@@ -48,7 +49,10 @@ class PyGNNWrapperTests(unittest.TestCase):
     agent = BehaviorGraphSACAgent(environment=env, params=params)
     env.ml_behavior = agent
     runner = SACRunner(params=params, environment=env, agent=agent)
-    return agent, runner, observer
+
+    # Test that agent is used
+    self.assertIsNotNone(agent)
+    self.assertIsNotNone(runner._agent)
 
   def test_gnn_parameters(self):
     params = ParameterServer()
@@ -56,20 +60,23 @@ class PyGNNWrapperTests(unittest.TestCase):
     params["ML"]["BehaviorGraphSACAgent"]["GNN"]["MpLayerNumUnits"] = 64
     params["ML"]["BehaviorGraphSACAgent"]["GNN"]["message_calculation_class"] = "gnn_edge_mlp"
     params["ML"]["BehaviorGraphSACAgent"]["GNN"]["global_exchange_mode"] = "mean"
+    gnn_library = GNNWrapper.SupportedLibrary.spektral # "tf2_gnn" or "spektral"
+    params["ML"]["BehaviorGraphSACAgent"]["GNN"]["library"] = gnn_library
     
     bp = ContinuousHighwayBlueprint(params, number_of_senarios=2500, random_seed=0)
     observer = GraphObserver(params=params)
     env = SingleAgentRuntime(blueprint=bp, observer=observer, render=False)
     sac_agent = BehaviorGraphSACAgent(environment=env, params=params)
 
-    actor_gnn = sac_agent._agent._actor_network._gnn._gnn
-    critic_gnn = sac_agent._agent._actor_network._gnn._gnn
+    actor_gnn = sac_agent._agent._actor_network._gnn
+    critic_gnn = sac_agent._agent._critic_network_1._gnn
 
     for gnn in [actor_gnn, critic_gnn]:
-      self.assertEqual(gnn._params["num_layers"], 4)
-      self.assertEqual(gnn._params["hidden_dim"], 64)
+      self.assertEqual(gnn._params["NumMpLayers"], 4)
+      self.assertEqual(gnn._params["MpLayerNumUnits"], 64)
       self.assertEqual(gnn._params["message_calculation_class"], "gnn_edge_mlp")
       self.assertEqual(gnn._params["global_exchange_mode"], "mean")
+      self.assertEqual(gnn._params["library"], gnn_library)
 
 if __name__ == '__main__':
   unittest.main()
