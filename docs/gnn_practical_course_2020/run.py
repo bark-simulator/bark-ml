698a51b5a0cf68671ff9add336354cab1cfc2dc5
TrungQuocNguyen <trungquoc.nguyen@tum.de>
2020-08-14 16:59:01 +0200

    Change the folder name in BUILD and run.py

--- docs/gnn_practical_course_2020/run.py
+++ docs/gnn_practical_course_2020/run.py
@@ -3,7 +3,7 @@ from notebook import notebookapp
 
 cwd = os.getcwd()
 runfiles_dir = cwd.split("run.runfiles")[0]
-bark_root = os.path.join(runfiles_dir, "run.runfiles/bark_ml/docs/gnn_practial_course_2020")
+bark_root = os.path.join(runfiles_dir, "run.runfiles/bark_ml/docs/gnn_practical_course_2020")
 
 print("Changing to bark root {}".format(bark_root))
 os.chdir(bark_root)
