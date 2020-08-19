86ba3ba9d87ba7ef7f6be600377f7ccd384484f8
patrickhart <patrickhart.1990@gmail.com>
2020-08-19 21:18:50 +0200

    Added comments

--- bark_ml/environments/counterfactual_runtime.py
+++ bark_ml/environments/counterfactual_runtime.py
@@ -4,6 +4,7 @@
 #
 # This work is licensed under the terms of the MIT license.
 # For a copy, see <https://opensource.org/licenses/MIT>.
+
 import time
 import numpy as np
 import logging
@@ -45,37 +46,66 @@ class CounterfactualRuntime(SingleAgentRuntime):
     """
     return SingleAgentRuntime.reset(self, scenario=scenario)
 
-  def GenerateCounterfactualWorlds(self):
-    return [self._world.Copy() for _ in range(0, 10)]
+  def ReplaceBehaviorModel(self, agent_id):
+    # NOTE: dummy implementation
+    return self._world.Copy()
+  
+  def GetAgentIds(self):
+    # NOTE: only use nearby agents
+    agent_ids = list(self._world.agents.keys())
+    eval_id = self._scenario._eval_agent_ids[0]
+    agent_ids.remove(eval_id)
+    return agent_ids
 
-  def SimulateWorld(self, world, N=5, **kwargs):
+  def GenerateCounterfactualWorlds(self):
+    cf_worlds = []
+    agent_ids = self.GetAgentIds()
+    for agent_id in agent_ids:
+      return_dict = {}
+      return_dict[agent_id] = self.ReplaceBehaviorModel(agent_id)
+      cf_worlds.append(return_dict)
+    return cf_worlds
+
+  def SimulateWorld(self, world, local_tracer, N=5, **kwargs):
     self.ml_behavior.set_action_externally = False
     eval_id = self._scenario._eval_agent_ids[0]
     self._world.agents[eval_id].behavior_model = self.ml_behavior
     for i in range(0, N):
       world.Step(self._step_time)
       eval_state = world.Evaluate()
-      self._tracer.Trace(eval_state, **kwargs)
+      local_tracer.Trace(eval_state, **kwargs)
     self.ml_behavior.set_action_externally = True
   
+  def St(self):
+    self._start_time = time.time()
+
+  def Et(self):
+    end_time = time.time()
+    dt = end_time - self._start_time
+    self._logger.info(f"It took {dt:.3f} seconds to simulate all" + \
+                      f" counterfactual worlds.")
+
   def step(self, action):
     """perform the cf evaluation"""
-    self._tracer = Tracer()
-    # NOTE: clone self._world M times
-    # NOTE: this should be a dict {"world_0": {exchanged_behaviors: }}
+    # simulate counterfactual worlds
+    local_tracer = Tracer()
+    self.St()
     cf_worlds = self.GenerateCounterfactualWorlds()
-    start_time = time.time()
-    for i, cf_world in enumerate(cf_worlds):
-      self.SimulateWorld(cf_world, N=5, num_episode=i)
-    end_time = time.time()
-    dt = end_time-start_time
-    self._logger.info(f"It took {dt:.3f} seconds to simulate the" + \
-                      f" counterfactual worlds.")
-    collision_rate = self._tracer.Query(
-      key="collision", group_by="num_episode", agg_type="MEAN").mean()
+    for cf_world in cf_worlds:
+      cf_key = list(cf_world.keys())[0]
+      self.SimulateWorld(
+        cf_world[cf_key], local_tracer, N=5, replaced_agent=cf_key)
+    self.Et()
+
+    # evaluate counterfactual worlds
+    collision_rate = local_tracer.Query(
+      key="collision", group_by="replaced_agent", agg_type="MEAN")
+    mean_collision_rate = collision_rate.mean()
     self._logger.info(f"The counterfactual worlds have a collision" + \
-                      f"-rate of {collision_rate:.3f}.")
-    if collision_rate > self._max_col_rate:
+                      f"-rate of {mean_collision_rate:.3f}.")
+    
+    # choose a policy
+    if mean_collision_rate > self._max_col_rate:
       # NOTE: assign a backup model if the collision-rate is too high
       pass
     return SingleAgentRuntime.step(self, action)
