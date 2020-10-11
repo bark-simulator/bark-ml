6798f0f5236e3ae31da3a2d89d2eeeb0c9be76ed
patrickhart <patrickhart.1990@gmail.com>
2020-10-11 20:59:43 +0200

    Basic experiment runner implementation

--- experiments/experiment_runner.py
+++ experiments/experiment_runner.py
@@ -1,14 +1,68 @@
-# always build hash when training, loading, saving
+import pprint
+from absl import app
+from absl import flags
+
+from experiments.experiment import Experiment
+
+FLAGS = flags.FLAGS
+flags.DEFINE_enum("mode",
+                  "visualize",
+                  ["train", "visualize", "evaluate"],
+                  "Mode the configuration should be executed in.")
+flags.DEFINE_string("exp_path",
+                    "/Users/hart/Development/bark-ml/experiments/data/config.json",
+                    "Path to the experiment json.")
+
 
 class ExperimentRunner:
-  def __init__(self, json_file):
-    self._experiment = None
+  def __init__(self, json_file, mode):
+    self._experiment_json = json_file
+    self._experiment = self.BuildExperiment(json_file)
+    self.Visitor(mode)
+
+  def Visitor(self, mode):
+    if mode == "train":
+      self.Train()
+    if mode == "visualize":
+      self.Visualize()
+    if mode == "evaluate":
+      self.Evaluate()
+  
+  def BuildExperiment(self, json_file):
+    return Experiment(json_file)
+  
+  def GenerateHash(self):
+    """hash-function to indicate whether the same json is used
+       as during training"""
+    ml_params = self._experiment.params.ConvertToDict()
+    return hash(frozenset(ml_params.items()))
 
   def Train(self):
-    pass
+    # save json + hash in training folder
+    self._experiment.params["ML"]["BehaviorTFAAgents"]["CheckpointPath"] = \
+      "/Users/hart/Development/bark-ml/checkpoints_merge_spektral_att3/"
+    self._experiment.params["ML"]["TFARunner"]["SummaryPath"] = \
+      "/Users/hart/Development/bark-ml/checkpoints_merge_spektral_att3/"
+    self._experiment.runner.SetupSummaryWriter()
+    self._experiment.runner.Train()
   
   def Evaluate(self):
-    pass
+    # check hash
+    self._experiment.runner.Run(num_episodes=5, render=False)
   
   def Visualize(self):
-    pass
+    # check hash
+    self._experiment.runner.Run(num_episodes=5, render=True)
+    
+  def SerializeExperiment(self):
+    return self._experiment.params.ParamsToDict()
+
+# run experiment
+def run_experiment(argv):
+  # experiment json, save path, mode
+  exp_runner = ExperimentRunner(
+    json_file=FLAGS.exp_path,
+    mode=FLAGS.mode)
+
+if __name__ == '__main__':
+  app.run(run_experiment)    
--- experiments/py_experiment_tests.py
+++ experiments/py_experiment_tests.py
@@ -28,9 +28,9 @@ from experiments.experiment import Experiment
 
 
 class PyExperimentTests(unittest.TestCase):
-  @unittest.skip("...")
+  @unittest.skip("..")
   def test_module_creation(self):
-    params = ParameterServer(filename="experiments/example_experiment/config.json")
+    params = ParameterServer(filename="experiments/data/config.json")
     # Blueprint
     ml_behavior = BehaviorContinuousML(params=params)
     module_name = "ConfigurableScenarioBlueprint"
@@ -62,10 +62,10 @@ class PyExperimentTests(unittest.TestCase):
     module_name = "SACRunner"
     runner = eval("{}(params=params, environment=runtime, \
                       agent=agent)".format(module_name))
-
-  @unittest.skip("...")
+  
+  @unittest.skip("..")
   def test_module_creation_from_json(self):
-    params = ParameterServer(filename="experiments/example_experiment/config.json")
+    params = ParameterServer(filename="experiments/data/config.json")
     exp_params = params["Experiment"]
     
     def LoadModule(module_name, dict_items):
@@ -118,14 +118,13 @@ class PyExperimentTests(unittest.TestCase):
   def test_experiment_class(self):
     experiment = Experiment("experiments/data/config.json")
     # visualize/evaluate
-    # experiment.runner.Run(num_episodes=5, render=True)
-    # train
-    experiment.params["ML"]["BehaviorTFAAgents"]["CheckpointPath"] = \
-      "/Users/hart/Development/bark-ml/checkpoints_merge_spektral_att3/"
-    experiment.params["ML"]["TFARunner"]["SummaryPath"] = \
-      "/Users/hart/Development/bark-ml/checkpoints_merge_spektral_att3/"
-    experiment.runner.SetupSummaryWriter()
-    experiment.runner.Train()
+    experiment.runner.Run(num_episodes=2, render=False)
+    # experiment.params["ML"]["BehaviorTFAAgents"]["CheckpointPath"] = \
+    #   "/Users/hart/Development/bark-ml/checkpoints_merge_spektral_att3/"
+    # experiment.params["ML"]["TFARunner"]["SummaryPath"] = \
+    #   "/Users/hart/Development/bark-ml/checkpoints_merge_spektral_att3/"
+    # experiment.runner.SetupSummaryWriter()
+    # experiment.runner.Train()
 
 if __name__ == '__main__':
   unittest.main()
