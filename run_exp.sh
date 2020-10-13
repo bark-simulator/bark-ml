3bd40597b01b30071772cbbceee1f79dfc99607f
patrickhart <patrickhart.1990@gmail.com>
2020-10-13 22:08:01 +0200

    export backend

--- run_exp.sh
+++ run_exp.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 cd /bark-ml/bark-ml
 virtualenv -p python3 ./bark_ml/python_wrapper/venv --system-site-packages
+export MPLBACKEND="agg"
 . ./bark_ml/python_wrapper/venv/bin/activate
 bazel run //experiments:experiment_runner -- $@
