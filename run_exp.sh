1e0d9a891995436c51e27e4a831c3498db7084c4
patrickhart <patrickhart.1990@gmail.com>
2020-10-13 21:50:04 +0200

    changed bash

--- run_exp.sh
+++ run_exp.sh
@@ -1,4 +1,5 @@
 #!bin/bash
 cd /bark-ml/bark-ml
 virtualenv -p python3 ./bark_ml/python_wrapper/venv --system-site-packages
+. ./bark_ml/python_wrapper/venv/bin/activate
 bazel run //experiments:experiment_runner -- $@
