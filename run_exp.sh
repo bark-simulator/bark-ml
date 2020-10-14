58b7850357187dbd3024a1087c470246c219b738
patrickhart <patrickhart.1990@gmail.com>
2020-10-14 17:10:25 +0200

    .

--- run_exp.sh
+++ run_exp.sh
@@ -3,4 +3,4 @@ cd /bark-ml/bark-ml
 virtualenv -p python3 ./bark_ml/python_wrapper/venv --system-site-packages
 export MPLBACKEND="agg"
 . ./bark_ml/python_wrapper/venv/bin/activate
-bazel --local_cpu_resources=4 run //experiments:experiment_runner -- $@
+bazel run --jobs=4 //experiments:experiment_runner -- $@
