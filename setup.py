dd0b15c91bb68684efe3aaa3b4ad181b07951ac7
patrickhart <patrickhart.1990@gmail.com>
2021-05-14 21:36:03 +0200

    plain path

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup, find_packages, Extension
 import sys, os
 
-
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 # A dummy native extension to mark module as platform specific
