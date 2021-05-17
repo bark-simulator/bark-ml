186305daa1faa6d7ede579ca29af8137279d3940
patrickhart <patrickhart.1990@gmail.com>
2021-05-17 11:39:01 +0200

    fix exp tests

--- experiments/py_experiment_tests.py
+++ experiments/py_experiment_tests.py
@@ -8,14 +8,17 @@
 
 
 import unittest
+import os
 from experiments.experiment import Experiment
 
 
 class PyExperimentTests(unittest.TestCase):
   def test_experiment_class(self):
-    experiment = Experiment("experiments/configs/highway_gnn.json")
+    experiment = Experiment(
+      os.path.join(os.path.dirname(__file__),
+      "experiments/configs/highway_gnn.json"))
     # visualize/evaluate
-    # experiment.runner.Run(num_episodes=1, render=False)
+    experiment.runner.Run(num_episodes=1, render=False)
     # experiment.params["ML"]["BehaviorTFAAgents"]["CheckpointPath"] = \
     #   "/Users/hart/Development/bark-ml/checkpoints_merge_spektral_att3/"
     # experiment.params["ML"]["TFARunner"]["SummaryPath"] = \
