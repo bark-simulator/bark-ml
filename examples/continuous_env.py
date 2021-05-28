25eec496356a85c0affda01107f6cc383427cd5c
patrickhart <patrickhart.1990@gmail.com>
2021-05-28 18:40:54 +0200

    Fix bark-ml

--- examples/continuous_env.py
+++ examples/continuous_env.py
@@ -19,6 +19,7 @@ env = gym.make("highway-v0")
 env.reset()
 done = False
 while done is False:
-  action = np.random.uniform(low=np.array([-0.5, -0.1]), high=np.array([0.5, 0.1]), size=(2, ))
+  action = np.array([0, 0])
+  # action = np.random.uniform(low=np.array([-0.5, -0.1]), high=np.array([0.5, 0.1]), size=(2, ))
   observed_next_state, reward, done, info = env.step(action)
   print(f"Observed state: {observed_next_state}, Action: {action}, Reward: {reward}, Done: {done}")
