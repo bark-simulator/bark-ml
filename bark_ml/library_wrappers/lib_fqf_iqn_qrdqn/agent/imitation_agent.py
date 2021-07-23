c3fd57c87f3c4933ac8e36a155ebfce009762dc4
Karin Frlic <karin.frlic@gmail.com>
2021-07-23 18:21:32 +0200

    Output intermediate losses before summing them up

--- bark_ml/library_wrappers/lib_fqf_iqn_qrdqn/agent/imitation_agent.py
+++ bark_ml/library_wrappers/lib_fqf_iqn_qrdqn/agent/imitation_agent.py
@@ -76,15 +76,17 @@ class BenchmarkSplitSupervisedLoss(TrainingBenchmark):
       converted_desired_values = self.agent.convert_values(action_values_desired)
       converted_current_values = self.agent.convert_values(action_values_current)
 
-      loss = self.agent.calculate_loss(converted_current_values,
-                                       converted_desired_values)
+      loss, intermediate_losses = self.agent.calculate_loss(converted_current_values,
+                                                            converted_desired_values,
+                                                            return_intermediate_losses=True)
 
-      return self.evaluate_loss(loss.item(), converted_current_values, converted_desired_values)
+      return self.evaluate_loss(loss.item(), converted_current_values, converted_desired_values,
+                                intermediate_losses=intermediate_losses)
 
   def evaluate_loss(self, scalar_loss, converted_current_values, converted_desired_values,
-                    phase="test", logits=False):
+                    phase="test", logits=False, intermediate_losses=None):
     result = {f"loss/{phase}": scalar_loss}
-    formatted_result = f"Loss = {scalar_loss}\n          | Mean SE | Var SE\n"
+    formatted_result = f"Loss = {scalar_loss}\n          | Mean SE      | Var SE\n"
 
     if logits:
       converted_current_values = apply_sigmoid_to_dict(converted_current_values)
@@ -99,7 +101,13 @@ class BenchmarkSplitSupervisedLoss(TrainingBenchmark):
       result[f"squared_error_mean/{key}/{phase}"] = error_mean
       result[f"squared_error_var/{key}/{phase}"] = error_var
 
-      formatted_result += f"{key:10}| {error_mean:7.3f} | {error_var:7.3f}\n"
+      formatted_result += f"{key:10}| {error_mean:12.8f} | {error_var:12.8f}\n"
+
+    if intermediate_losses is not None:
+      formatted_result += "Intermediate losses:\n"
+      for value_function, intermediate_loss in intermediate_losses.items():
+        result[f"intermediate_loss/{value_function}/{phase}"] = intermediate_loss
+        formatted_result += f"{value_function:10}| {intermediate_loss:12.8f}\n"
 
     if self.gradients_and_weights_dir:
       self.save_gradients_and_weights_to_file(
@@ -231,7 +239,8 @@ class ImitationAgent(BaseAgent):
 
     return states, action_values
 
-  def calculate_loss(self, action_values_current, action_values_desired, logits=False):
+  def calculate_loss(self, action_values_current, action_values_desired, logits=False,
+                     return_intermediate_losses=False):
     """
     If logits=True, the loss must use the sigmoid function before comparing
     current values to the desired values.
@@ -244,7 +253,8 @@ class ImitationAgent(BaseAgent):
       nans_desired = torch.isnan(action_values_desired[key]).nonzero(as_tuple=False)
       action_values_desired[key][nans_desired] = action_values_current[key][nans_desired].detach().clone()
 
-    loss = self.selected_loss(action_values_current, action_values_desired, logits)
+    loss = self.selected_loss(action_values_current, action_values_desired, logits,
+                              return_intermediate_losses=return_intermediate_losses)
     return loss
 
   def calculate_actions(self, state):
--- bark_ml/library_wrappers/lib_fqf_iqn_qrdqn/agent/loss/loss_function.py
+++ bark_ml/library_wrappers/lib_fqf_iqn_qrdqn/agent/loss/loss_function.py
@@ -19,25 +19,30 @@ class Loss:
     self._criterion = criterion
     self._weights = weights
 
-  def __call__(self, current_values, desired_values, logits):
+  def __call__(self, current_values, desired_values, logits, return_intermediate_losses=False):
     """
     Calculates loss.
 
     Arguments current_values and desired_values are expected to be
     dictionaries.
+
+    If return_intermediate_losses==True, return not only the weighted loss,
+    but also a dictionary with a loss for each of the value functions, e.g.:
+      {"Return": 0.03, "Envelope": 0.02, "Collision": 0.01}
     """
     if logits:
       # Transform raw output to values between 0 and 1
       current_values = apply_sigmoid_to_dict(current_values)
 
-    return self._calculate_weighted_loss(current_values, desired_values,
-                                         logits)
+    return self._calculate_weighted_loss(current_values, desired_values, logits,
+                                         return_intermediate_losses=return_intermediate_losses)
 
   def _select_criterion(self, _):
     return self._criterion
 
-  def _calculate_weighted_loss(self, current_values, desired_values, logits):
-    losses = []
+  def _calculate_weighted_loss(self, current_values, desired_values, logits,
+                               return_intermediate_losses=False):
+    losses = {}
     weights_sum = 0
     criterion = self._select_criterion(logits)
 
@@ -46,9 +51,14 @@ class Loss:
       weights_sum += weight
 
       loss = criterion(current_values[key], desired_values[key])
-      losses.append(weight * loss)
+      losses[key] = weight * loss
 
-    return sum(losses) / weights_sum
+    weighted_loss = sum(losses.values()) / weights_sum
+
+    if return_intermediate_losses:
+      return weighted_loss, losses
+
+    return weighted_loss
 
 
 class LossMSE(Loss):
@@ -68,16 +78,20 @@ class LossBCE(Loss):
       return self._criterion_logits
     return self._criterion
 
-  def __call__(self, current_values, desired_values, logits):
-    return self._calculate_weighted_loss(current_values, desired_values,
-                                         logits)
+  def __call__(self, current_values, desired_values, logits, return_intermediate_losses=False):
+    return self._calculate_weighted_loss(current_values, desired_values, logits,
+                                         return_intermediate_losses=return_intermediate_losses)
+
 
 class LossPolicyCrossEntropy(Loss):
   def __init__(self):
     pass
 
-  def __call__(self, current_values, desired_values, logits):
+  def __call__(self, current_values, desired_values, logits, return_intermediate_losses=False):
     logsoftmax = nn.LogSoftmax(dim=1)
     target = desired_values["Policy"]
     pred = current_values["Policy"]
-    return torch.mean(torch.sum(-target*logsoftmax(pred), 1))
+    loss = torch.mean(torch.sum(-target*logsoftmax(pred), 1))
+    if return_intermediate_losses:  # Added to match the interface of other losses
+      return loss, None
+    return loss
