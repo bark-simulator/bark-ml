681e912ce3af68f347845c3599453483664950a4
patrickhart <patrickhart.1990@gmail.com>
2021-10-12 10:13:18 +0200

    cleanup observer

--- bark_ml/observers/frenet_observer.py
+++ bark_ml/observers/frenet_observer.py
@@ -10,6 +10,7 @@
 from gym import spaces
 import numpy as np
 from bark.runtime.commons.parameters import ParameterServer
+from bark.core.models.dynamic import StateDefinition
 from bark.core.geometry import *
 
 # bark-ml
@@ -21,6 +22,18 @@ class FrenetObserver(BaseObserver):
 
   def __init__(self, params=ParameterServer()):
     BaseObserver.__init__(self, params)
+    self._num_other_agents = \
+      self._params["ML"]["FrenetObserver"]["NumOtherAgents",
+      "Number of other agents",
+      1]
+    self._road_norm_width = \
+      self._params["ML"]["FrenetObserver"]["RoadNormWidth",
+      "Road width.",
+      4.8]
+    self._max_vel = \
+      self._params["ML"]["FrenetObserver"]["MaxVel",
+      "Max. velocity",
+      15.]
 
   def getLaneCorr(self, observed_world):
     rc = observed_world.ego_agent.road_corridor
@@ -40,25 +53,50 @@ class FrenetObserver(BaseObserver):
       center_line, Point2d(current_state[1], current_state[2]))
     return s
 
+  def transformAgentToFrenet(self, observed_world, current_state):
+    d = self.getD(observed_world, current_state)
+    s = self.getS(observed_world, current_state)
+    lc = self.getLaneCorr(observed_world)
+    s = self._norm_to_range(s, [0, lc.center_line.Length()])
+    d = self._norm_to_range(
+      d, [-self._road_norm_width, self._road_norm_width])
+    v = self._norm_to_range(s, [0, self._max_vel])
+    theta = self._norm_to_range(s, [0, 6.3])
+    return [s, d, theta, v]
+
+  def getNearbyAgents(self, observed_world, state):
+    ego_position = Point2d(
+      state[StateDefinition.X_POSITION],
+      state[StateDefinition.Y_POSITION])
+    return observed_world.GetNearestAgents(
+      ego_position, self._num_other_agents + 1)
+
+  @staticmethod
+  def flatten(t):
+    return [item for sublist in t for item in sublist]
+
   def Observe(self, observed_world):
     """See base class."""
     current_state = observed_world.ego_agent.state
-    d = self.getD(observed_world, current_state)
-    s = self.getS(observed_world, current_state)
-    # TODO(@hart): make more generic; for now hack
-    lc = self.getLaneCorr(observed_world)
-    s /= lc.center_line.Length()
-    d /= -4.
-    v = current_state[4]/15.
-    theta = (current_state[3])/6.28
-    state = np.array([s, d, theta, v], dtype=np.float32)
+    observed_states = []
+    ego_frenet = self.transformAgentToFrenet(
+      observed_world, current_state)
+    observed_states.append(ego_frenet)
+    nearby_agents = self.getNearbyAgents(observed_world, current_state)
+    for agent_id, agent in nearby_agents.items():
+      if agent_id != observed_world.ego_agent.id:
+        other_state = self.transformAgentToFrenet(observed_world, agent.state)
+        observed_states.append(other_state)
+    flat_observation = self.flatten(observed_states)
+    state = np.array(flat_observation, dtype=np.float32)
     return state
 
   @property
   def observation_space(self):
     return spaces.Box(
-      low=np.zeros(4),
-      high = np.ones(4))
+      low=np.zeros(4*(self._num_other_agents+1)),
+      high = np.ones(4*(self._num_other_agents+1))
+    )
 
   @staticmethod
   def _norm_to_range(value, range):
