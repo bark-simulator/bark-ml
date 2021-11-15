025cdcd433e3fd6e8ebe1157be61e9bc6118ac90
patrickhart <patrickhart.1990@gmail.com>
2021-11-15 11:30:37 +0100

    Modify unit test

--- bark_ml/experiment/py_experiment_tests.py
+++ bark_ml/experiment/py_experiment_tests.py
@@ -10,22 +10,20 @@
 import unittest
 import os
 from bark_ml.experiment.experiment import Experiment
+from bark_ml.experiment.experiment_runner import ExperimentRunner
 
 
 class PyExperimentTests(unittest.TestCase):
-  def test_experiment_class(self):
+  def test_experiment(self):
     experiment = Experiment(
       os.path.join(os.path.dirname(__file__),
-      "configs/highway_gnn.json"))
-    # visualize/evaluate
-    # experiment.runner.Run(num_episodes=1, render=False)
-    # experiment.params["ML"]["BehaviorTFAAgents"]["CheckpointPath"] = \
-    #   "/Users/hart/Development/bark-ml/checkpoints_merge_spektral_att3/"
-    # experiment.params["ML"]["TFARunner"]["SummaryPath"] = \
-    #   "/Users/hart/Development/bark-ml/checkpoints_merge_spektral_att3/"
-    # experiment.runner.SetupSummaryWriter()
-    # experiment.runner.Train()
-    pass
+      "data/highway_gnn.json"))
+
+  def test_experiment_runner(self):
+    exp_runner = ExperimentRunner(
+      json_file=os.path.join(os.path.dirname(__file__),
+      "data/highway_gnn.json"), mode="evaluate", random_seed=0)
+
 
 if __name__ == '__main__':
   unittest.main()
