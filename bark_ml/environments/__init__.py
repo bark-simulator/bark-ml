402e173a1d321f79e8fc26063bd41db19228b726
patrickhart <patrickhart.1990@gmail.com>
2022-01-18 13:45:13 +0100

    Add combined noise runtime

--- bark_ml/environments/__init__.py
+++ bark_ml/environments/__init__.py
@@ -1,3 +1,4 @@
 from bark_ml.environments.single_agent_runtime import SingleAgentRuntime  # pylint: disable=unused-import
 from bark_ml.environments.counterfactual_runtime import CounterfactualRuntime  # pylint: disable=unused-import
-from bark_ml.environments.modified_single_agent_runtimes import SingleAgentDelayRuntime, SingleAgentGaussianRuntime  # pylint: disable=unused-import
+from bark_ml.environments.modified_single_agent_runtimes import SingleAgentDelayRuntime, \
+    SingleAgentGaussianNoiseRuntime, SingleAgentDelayAndGaussianNoiseRuntime  # pylint: disable=unused-import
--- bark_ml/environments/modified_single_agent_runtimes.py
+++ bark_ml/environments/modified_single_agent_runtimes.py
@@ -51,7 +51,7 @@ class SingleAgentDelayRuntime(SingleAgentRuntime):
     return super().reset(scenario)
 
 
-class SingleAgentGaussianRuntime(SingleAgentRuntime):
+class SingleAgentGaussianNoiseRuntime(SingleAgentRuntime):
   """An environment that executes actions with noise.
   """
   def __init__(self,
@@ -72,13 +72,57 @@ class SingleAgentGaussianRuntime(SingleAgentRuntime):
                      viewer=viewer,
                      scenario_generator=scenario_generator,
                      render=render)
-    self._sigmas = sigmas or [4., 1.]
+    self._sigmas = sigmas or [0., 0.001]
 
   def step(self, action):
     action += np.random.multivariate_normal(
       mean=list(action), cov=np.diag(self._sigmas))
+    # TODO: use params
+    action = np.clip(action, [-4, -0.1], [4, 0.1])
     return super().step(action)
 
-  def reset(self, scenario=None):
-    return super().reset(scenario)
 
+class SingleAgentDelayAndGaussianNoiseRuntime(SingleAgentRuntime):
+  """An environment that executes actions with a pre-defined
+     delay.
+  """
+  def __init__(self,
+               blueprint=None,
+               ml_behavior=None,
+               observer=None,
+               evaluator=None,
+               step_time=None,
+               viewer=None,
+               scenario_generator=None,
+               render=False,
+               default_action=None,
+               num_delay_steps=None,
+               sigmas=None):
+    super().__init__(blueprint=blueprint,
+                     ml_behavior=ml_behavior,
+                     observer=observer,
+                     evaluator=evaluator,
+                     step_time=step_time,
+                     viewer=viewer,
+                     scenario_generator=scenario_generator,
+                     render=render)
+    self._default_action = default_action or [0., 0.]
+    self._num_delay_steps = num_delay_steps or 5
+    self._sigmas = sigmas or [0., 0.001]
+    self._action_queue = queue.Queue()
+
+  def step(self, action):
+    self._action_queue.put(action)
+    action_to_execute = self._action_queue.get()
+    action_to_execute += np.random.multivariate_normal(
+      mean=list(action_to_execute), cov=np.diag(self._sigmas))
+    # TODO: use params
+    action_to_execute = np.clip(action_to_execute, [-4, -0.1], [4, 0.1])
+    return super().step(action_to_execute)
+
+  def reset(self, scenario=None):
+    while not self._action_queue.empty():
+      self._action_queue.get()
+    for _ in range(0, self._num_delay_steps):
+      self._action_queue.put(np.array(self._default_action))
+    return super().reset(scenario)
